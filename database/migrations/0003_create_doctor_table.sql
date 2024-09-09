af0ab4602ee96af4e1675bbdfc544dc27a6e2ac9
diego <diegoa2992@gmail.com>
2024-09-09 17:10:44 -0500

    fixed entries

--- database/migrations/0003_create_doctor_table.sql
+++ database/migrations/0003_create_doctor_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS doctor (
     doctor_id SERIAL PRIMARY KEY,
     name name,
-    license_number VARCHAR(50),
+    license_number VARCHAR(50) NOT NULL,
     yoe INTEGER,
     -- years of experience
     specialty VARCHAR(50) [],
