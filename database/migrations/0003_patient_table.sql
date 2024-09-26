14e0ac6aec7eda4de455873fcb0d57749ee8ea4f
diego <diegoa2992@gmail.com>
2024-09-26 18:41:44 -0500

    diego finished tables

---
+++ database/migrations/0003_patient_table.sql
@@ -0,0 +1,9 @@
+-- holds a ptr to the user_id to distinguish between patients and doctors
+-- we could maybe do a boolcase ? ie ids larger than 1000 are doctors ? 
+-- patient table, extends user
+CREATE TABLE IF NOT EXISTS patients (
+    patient_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id INTEGER UNIQUE NOT NULL,
+    patient_name VARCHAR(50) NOT NULL,
+    emergency_contacts JSON 
+);
