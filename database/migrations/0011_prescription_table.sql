a2c60b68ce6ae2a6ec1a40d18c42374b40d15564
JohnBunger <iamtheonlypro@gmail.com>
2024-09-28 17:51:09 -0500

    11 done c:

---
+++ database/migrations/0011_prescription_table.sql
@@ -0,0 +1,13 @@
+-- prescription information linked to a medical record
+CREATE TABLE IF NOT EXISTS prescription (
+    prescription_id INT PRIMARY KEY AUTO_INCREMENT,
+    -- record_id INTEGER NOT NULL,     -- record_id INT NOT NULL,
+    medication_info TEXT NOT NULL,
+    date_issued TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    pharmacy_details JSON,
+
+--     CONSTRAINT fk_prescription_medical_record
+--         FOREIGN KEY (record_id) 
+--         REFERENCES medical_records(record_id)
+--         ON DELETE CASCADE
+-- );
---
+++ database/migrations/0012_audit_log_table.sql
@@ -0,0 +1,19 @@
+-- audit log to track changes in system
+CREATE TABLE IF NOT EXISTS AUDIT_LOG(
+   log_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+   table_name VARCHAR(30) NOT NULL,
+   record_id INTEGER NOT NULL,
+   audit_action VARCHAR(10) NOT NULL,
+   changed_by INTEGER NOT NULL,
+   changed_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+   old_values JSONB,
+   new_values JSONB,
+
+   CONSTRAINT fk_audit_log_user
+      FOREIGN KEY (changed_by) 
+      REFERENCES users(user_id)
+      ON DELETE SET NULL
+);
+
+-- need to implement trigger function to log changes to the audit log table
+-- CREATE OR REPLACE FUNCTION audit_log_trigger() RETURNS TRIGGER AS $$
