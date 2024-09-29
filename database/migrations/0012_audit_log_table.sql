f03e6a0874ddffcc2e34bf6cdb769551eae02ba2
JohnBunger <iamtheonlypro@gmail.com>
2024-09-29 05:12:00 -0500

    12 and 13 foreign key maybe?

--- database/migrations/0012_audit_log_table.sql
+++ database/migrations/0012_audit_log_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS AUDIT_LOG(
    table_name VARCHAR(30) NOT NULL,
    record_id INT NOT NULL,
    audit_action VARCHAR(10) NOT NULL,
-   changed_by INT NOT NULL,
+   -- changed_by INT NOT NULL,
    changed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
    old_values JSON,
    new_values JSON,
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS AUDIT_LOG(
 --       FOREIGN KEY (changed_by) 
 --       REFERENCES users(user_id)
 --       ON DELETE SET NULL
--- );
+);
 
 -- need to implement trigger function to log changes to the audit log table
 -- CREATE OR REPLACE FUNCTION audit_log_trigger() RETURNS TRIGGER AS $$
--- database/migrations/0013_notifications_table.sql
+++ database/migrations/0013_notifications_table.sql
@@ -1,8 +1,8 @@
 -- notifications for UI
 CREATE TABLE /*IF NOT EXISTS*/ notifications (
     notification_id INT PRIMARY KEY AUTO_INCREMENT,
-    sender_id INT NOT NULL,
-    receiver_id INT NOT NULL,
+    -- sender_id INT NOT NULL,
+    -- receiver_id INT NOT NULL,
     type_notif ENUM('APPOINTMENT REMINDER', 'TEST RESULT AVAILABLE', 'PRESCRIPTION READY', 'BILLING REMINDER', 'GENERAL') NOT NULL,
     message TEXT NOT NULL,
     created_at DATE DEFAULT CURRENT_DATE NOT NULL,
