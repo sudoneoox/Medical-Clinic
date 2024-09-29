ad8c392f19c9a6da1464ad5a47e8cbf4ef9fe9df
JohnBunger <iamtheonlypro@gmail.com>
2024-09-29 05:00:34 -0500

    11 accounts for custom now
    didnt account for the custom type of medication before, now i do with the values all within prescription

--- database/migrations/0011_prescription_table.sql
+++ database/migrations/0011_prescription_table.sql
@@ -1,8 +1,11 @@
 -- prescription information linked to a medical record
 CREATE TABLE IF NOT EXISTS prescription (
     prescription_id INT PRIMARY KEY AUTO_INCREMENT,
-    -- record_id INTEGER NOT NULL,     -- record_id INT NOT NULL,
-    medication_info TEXT NOT NULL, -- changing next commit to account for custom type
+    -- record_id INT NOT NULL,
+    medication_name VARCHAR(50) NOT NULL,
+    dosage VARCHAR(20) NOT NULL,
+    frequency VARCHAR(20) NOT NULL,
+    duration VARCHAR(50) NOT NULL,  -- MySQL doesn't have an INTERVAL type
     date_issued TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     pharmacy_details JSON,
 
