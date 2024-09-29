532b9380a71aacf5a00327fde62b53ca5fde3b13
DanBrrl <funt.daniilbondar@gmail.com>
2024-09-29 15:23:37 -0500

    Changes from PostgreSQL to MySQL

---
+++ database/migrations/0022_doctor_specialties_junction.sql
@@ -0,0 +1,14 @@
+-- many to many relationship between doctors and specialties
+CREATE TABLE IF NOT EXISTS doctor_specialties (
+    doctor_id INT,
+    specialty_id INT,
+    PRIMARY KEY (doctor_id, specialty_id),
+    CONSTRAINT fk_doctor_specialties_doctor
+        FOREIGN KEY (doctor_id) 
+        REFERENCES doctor(doctor_id)
+        ON DELETE CASCADE,
+    CONSTRAINT fk_doctor_specialties_specialty
+        FOREIGN KEY (specialty_id)
+        REFERENCES specialties(specialty_id)
+        ON DELETE CASCADE
+);
---
+++ database/migrations/0026_remove_doctor_speciality_id.sql
@@ -0,0 +1,5 @@
+ALTER TABLE doctor
+DROP COLUMN specialty_id;
+
+ALTER TABLE doctor
+DROP FOREIGN KEY fk_doctor_specialty;
