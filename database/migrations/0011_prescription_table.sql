fac23cb4fd0c4c9cecb4040962f693ae1f26a8ac
JohnBunger <iamtheonlypro@gmail.com>
2024-09-29 04:51:44 -0500

    13 done c:
    Changing 11 next commit to fix approach to custom type from before

--- database/migrations/0011_prescription_table.sql
+++ database/migrations/0011_prescription_table.sql
@@ -2,7 +2,7 @@
 CREATE TABLE IF NOT EXISTS prescription (
     prescription_id INT PRIMARY KEY AUTO_INCREMENT,
     -- record_id INTEGER NOT NULL,     -- record_id INT NOT NULL,
-    medication_info TEXT NOT NULL,
+    medication_info TEXT NOT NULL, -- changing next commit to account for custom type
     date_issued TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     pharmacy_details JSON,
 
@@ -10,4 +10,4 @@ CREATE TABLE IF NOT EXISTS prescription (
 --         FOREIGN KEY (record_id) 
 --         REFERENCES medical_records(record_id)
 --         ON DELETE CASCADE
--- );
+);
