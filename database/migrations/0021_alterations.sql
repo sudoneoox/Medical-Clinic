e6baf26f759d50384b2b23f0cdb4f9edf2084976
DanBrrl <funt.daniilbondar@gmail.com>
2024-09-29 19:41:13 -0500

    fk comment out
    25 and 26 reference other tables

--- database/migrations/0021_alterations.sql
+++ database/migrations/0021_alterations.sql
@@ -1,21 +1,20 @@
-
 -- modify medical record to allow nurses to update/create records
 ALTER TABLE medical_records
 ADD COLUMN created_by INT,
 ADD COLUMN updated_by INT,
---ADD CONSTRAINT fk_medical_records_created_by
-    --FOREIGN KEY (created_by)
-    --REFERENCES users(user_id)
-    --ON DELETE SET NULL,
---ADD CONSTRAINT fk_medical_records_updated_by
-    --FOREIGN KEY (updated_by)
-    --REFERENCES users(user_id)
-    --ON DELETE SET NULL;
+-- ADD CONSTRAINT fk_medical_records_created_by
+--     FOREIGN KEY (created_by)
+--     REFERENCES users(user_id)
+--     ON DELETE SET NULL,
+-- ADD CONSTRAINT fk_medical_records_updated_by
+--     FOREIGN KEY (updated_by)
+--     REFERENCES users(user_id)
+--     ON DELETE SET NULL;
 
 -- modify billing to allow receptionists to update
-ALTER TABLE billing
-ADD COLUMN handled_by INT,
---ADD CONSTRAINT fk_billing_handled_by
-    --FOREIGN KEY (handled_by)
-    --REFERENCES users(user_id)
-    --ON DELETE SET NULL;
+-- ALTER TABLE billing
+-- ADD COLUMN handled_by INT,
+-- ADD CONSTRAINT fk_billing_handled_by
+--     FOREIGN KEY (handled_by)
+--     REFERENCES users(user_id)
+--     ON DELETE SET NULL;
