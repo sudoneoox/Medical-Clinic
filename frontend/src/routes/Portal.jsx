a365a653e4d3c8884c5fc4daad646639e9c07e2c
diego laptop <diegoa2992@gmail.com>
2024-10-10 17:33:29 -0500

    breakpoint

--- frontend/src/routes/Portal.jsx
+++ frontend/src/routes/Portal.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import "../styles/tailwindbase.css";
 
-const Provider = () => {
+// use components to fill the dashboard instead of making different pages for each
+// each component would need to be filled with the respective data received from the backend ie so calendar 
+// component for patient should be called in within the patient function return (<Calendar data={}/>) etc
+const Doctor = () => {
   return (
     <div>
       <h1>Provider Portal</h1>
       <p>Provider Portal</p>
     </div>
   );
+};
+
+const Receptionist = () => {
+  return <></>;
+};
+
+const Nurse = () => {
+  return <></>;
 };
 
 const Patient = () => {
@@ -19,6 +30,7 @@ const Patient = () => {
   );
 };
 
+// maybe implement?
 const Admin = () => {
   return (
     <div>
@@ -35,8 +47,14 @@ export default function Portal() {
 
   let portalComponent;
   switch (userType) {
-    case "provider":
-      portalComponent = <Provider />;
+    case "docotr":
+      portalComponent = <Doctor />;
+      break;
+    case "nurse":
+      portalComponent = <Nurse />;
+      break;
+    case "receptionist":
+      portalComponent = <Receptionist />;
       break;
     case "patient":
       portalComponent = <Patient />;
