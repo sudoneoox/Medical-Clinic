d10480303fe88fcf75fc2e66b699f51e367f270c
DanBrrl <funt.daniilbondar@gmail.com>
2024-10-18 17:37:24 -0500

    added text and background

--- frontend/src/routes/Contact.jsx
+++ frontend/src/routes/Contact.jsx
@@ -2,26 +2,47 @@ import React from "react";
 import '../styles/tailwindbase.css'
 import Navbar from "../components/Navbar";
 
+
 export default function ContactPage() {
   return (
     <> <Navbar />
-    <main className="container mx-auto mt-20 px-4">
-      
-        <div id="contact-page" >
+    <main className="container mx-auto mt-20 px-4" >
+<div
+  className="w-screen h-screen bg-cover bg-center "
+  style={{ backgroundImage: 'url("images/about_logo.png")' }}
+>
+<div id="contact-page" >
           <h1 className="text-2xl font-bold mb-4">Contact Us</h1>
-          <p className="text-center">
-            !!!We are a medical clinic that provides a variety of services to our patients.
+          <div className="text-2xl font-semibold mb-4 text-center">
+          <p>
+            At WSYK Medical center, we are dedicated to providing the highest level of care to our patients. 
+            </p>
+          <p>If you have any questions, need assistance, or would like to schedule an appointment, </p>
+          <p>feel free to reach out to us through any of the following methods.
+          </p> </div>
+          <div>
+          <p className="text-2xl font-semibold">
+          Address:
           </p>
-          <p className="text-center">
-          Texas Children's Hospital
-Texas Children's Hospital Wallace Tower
-Texas Medical Center
-6701 Fannin Street
-Houston, TX 77030
-
-Tel. 832-824-1000
-          </p>
+          <p>6701 Fannin Street</p>
+          <p>Houston, TX 77030</p>
+          </div>
+          <div>
+          <p className="text-2xl font-semibold">Phone:</p>
+          <p>Call us at: (123) 456-7890</p>
+          <p>Houston, TX 77030</p>
+          <p>After medical service number: (123) 456-7891 (24/7)</p>
+          </div>
+          <div>
+          <p className="text-2xl font-semibold">Emergency Services:</p>
+          <p>For medical emergencies, please call <span style={{ fontSize: '20px' }}> 911</span> or visit the nearest emergency room.</p>
+          <p>Our after-hours line is available for urgent concerns outside our normal business hours.</p>
+          </div>
         </div>
+</div>
+
+      
+        
       </main>
   </>
   );
