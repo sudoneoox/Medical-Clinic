8a36de2539f9d79d9e6409a2e8c513e93741df95
diego <diegoa2992@gmail.com>
2024-10-18 16:50:23 -0500

    diego database breakpoint for dashborad

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -157,11 +157,11 @@ const loginUser = async (req, res) => {
     const validPassword = user.user_password === password ? true: false;
     console.log("found user: ", user);
     if (!user) {
-      return res.status(401).json({ message: "email does not exist" }); 
+      return res.status(401).json({ message: "Email does not exist" }); 
     }
 
     if ((!validPassword)){
-      return res.status(402).json({message: "incorrect password "});
+      return res.status(401).json({message: "Incorrect Password "});
     }
 
 
