502ddf13a3f2e0994ef02866bbbc58bb7e978303
X0@laptop <diegoa2992@gmail.com>
2024-11-12 18:41:15 -0600

    remove unneeded triggers

--- database/scripts/combined_migrations.sql
+++ database/scripts/combined_migrations.sql
@@ -1245,20 +1245,6 @@ CALL populate_test_data(
 CALL populate_all_doctors_availability();
 ;
 DELIMITER //
-CREATE TRIGGER before_notification_insert 
-BEFORE INSERT ON notifications
-FOR EACH ROW
-BEGIN
-    IF NEW.expires_at IS NULL THEN
-        SET NEW.expires_at = DATE_ADD(NEW.created_at, INTERVAL 30 DAY);
-    END IF;
-    IF NEW.scheduled_for IS NOT NULL AND NEW.scheduled_for < NEW.created_at THEN
-        SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Notification cannot be scheduled for a past date';
-    END IF;
-END //
-DELIMITER ;
-DELIMITER //
 CREATE TRIGGER before_appointment_specialist_check
 BEFORE INSERT ON appointments
 FOR EACH ROW
